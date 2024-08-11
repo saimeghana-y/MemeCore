5965f79689d6f95a5110761074e0c9f11fd60500
Meghana <meghana@Meghanas-MacBook-Air.local>
2024-08-12 01:13:04 +0530

    vercel fix

--- app/editor/page.jsx
+++ app/editor/page.jsx
@@ -2,10 +2,20 @@
 
 import { useSearchParams } from 'next/navigation';
 import Editor from './Editor';
+import { useEffect, useState } from 'react';
 
 export default function EditorPage() {
   const searchParams = useSearchParams();
   const templateId = searchParams.get('templateId');
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true); // Set to true after component mounts
+  }, []);
+
+  if (!isClient) {
+    return null; // Prevent rendering on the server
+  }
 
   if (!templateId) {
     return <div>No template ID provided</div>;
